c5ec54a9b33aecc18aa4f5609c707d3ee0b19972
B612-jujin <jujingood@gmail.com>
2025-08-25 11:51:21 +0900

    Demo JDBC

---
+++ src/main/java/kr/ac/kopo/cjj/springboot_jdbctest/domain/Person.java
@@ -0,0 +1,12 @@
+package kr.ac.kopo.cjj.springboot_jdbctest.domain;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+
+}
